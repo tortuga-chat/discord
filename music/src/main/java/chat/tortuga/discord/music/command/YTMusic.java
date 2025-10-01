4341e009eefebad82a24285f85e7b40da8670afc
pepe <pedrohostert@proton.me>
2025-10-01 02:10:27 -0300

    Use prefixes string from YoutubeAudioSourceManager

--- music/src/main/java/chat/tortuga/discord/music/command/YTMusic.java
+++ music/src/main/java/chat/tortuga/discord/music/command/YTMusic.java
@@ -2,16 +2,16 @@ package chat.tortuga.discord.music.command;
 
 import chat.tortuga.discord.music.config.Music;
 import chat.tortuga.discord.music.service.MusicService;
+import dev.lavalink.youtube.YoutubeAudioSourceManager;
 import jakarta.enterprise.context.Dependent;
 
 @Dependent
 public class YTMusic extends AbstractSearchCommand {
 
-    private static final String SEARCH_PREFIX = "ytmsearch:";
     private final Music music;
 
     public YTMusic(MusicService service, Music music) {
-        super(service, SEARCH_PREFIX, "ytmusic", "Searches for a track in youtube music");
+        super(service, YoutubeAudioSourceManager.MUSIC_SEARCH_PREFIX, "ytmusic", "Searches for a track in youtube music");
         this.music = music;
     }
 
--- music/src/main/java/chat/tortuga/discord/music/command/Youtube.java
+++ music/src/main/java/chat/tortuga/discord/music/command/Youtube.java
@@ -2,16 +2,16 @@ package chat.tortuga.discord.music.command;
 
 import chat.tortuga.discord.music.config.Music;
 import chat.tortuga.discord.music.service.MusicService;
+import dev.lavalink.youtube.YoutubeAudioSourceManager;
 import jakarta.enterprise.context.Dependent;
 
 @Dependent
 public class Youtube extends AbstractSearchCommand {
 
-    private static final String SEARCH_PREFIX = "ytsearch:";
     private final Music music;
 
     public Youtube(MusicService service, Music music) {
-        super(service, SEARCH_PREFIX, "youtube", "Searches for a track in youtube");
+        super(service, YoutubeAudioSourceManager.SEARCH_PREFIX, "youtube", "Searches for a track in youtube");
         this.music = music;
     }
 
